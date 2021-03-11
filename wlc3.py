0f2130697649b8ea26529260f98e49ee789f1137
masti01 <mastigm@gmail.com>
2021-03-11 18:25:59 +0100

    wlc: test print correction

--- wlc3.py
+++ wlc3.py
@@ -2263,7 +2263,7 @@ class LinkCheckThread(threading.Thread):
         ok = False
         exception = False
         ignore = False
-        pywikibot.output('[{0}] :Processing URL {0} in page {1}'
+        pywikibot.output('[{}] :Processing URL {} in page {}'
                          .format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), self.url,
                                  self.page.title()))
         try:
@@ -2279,7 +2279,7 @@ class LinkCheckThread(threading.Thread):
         except (pywikibot.exceptions.FatalServerError, requests.ConnectionError, Exception):
             exception = True
             message = 'Exception while connecting.'
-            pywikibot.output('[{0}] Exception while processing URL {0} in page {1}'
+            pywikibot.output('[{}] Exception while processing URL {} in page {}'
                              .format(datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), self.url,
                                      self.page.title()))
             raise
