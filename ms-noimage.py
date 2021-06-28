3f4ca3414640fca7076fb58fe0e64f9addbddf5d
masti01 <mastigm@gmail.com>
2021-06-28 22:08:44 +0200

    ms-noimage: updtaed script for ementeries without image

---
+++ ms-noimage.py
@@ -0,0 +1,217 @@
+#!/usr/bin/python
+"""
+An incomplete sample script.
+
+This is not a complete bot; rather, it is a template from which simple
+bots can be made. You can rename it to mybot.py, then edit it in
+whatever way you want.
+
+Use global -simulate option for test purposes. No changes to live wiki
+will be done.
+
+
+The following parameters are supported:
+
+-always           The bot won't ask for confirmation when putting a page
+
+-text:            Use this text to be added; otherwise 'Test' is used
+
+-replace:         Don't add text but replace it
+
+-top              Place additional text on top of the page
+
+-summary:         Set the action summary message for the edit.
+
+All settings can be made either by giving option with the command line
+or with a settings file which is scripts.ini by default. If you don't
+want the default values you can add any option you want to change to
+that settings file below the [basic] section like:
+
+    [basic] ; inline comments starts with colon
+    # This is a commend line. Assignments may be done with '=' or ':'
+    text: A text with line break and
+        continuing on next line to be put
+    replace: yes ; yes/no, on/off, true/false and 1/0 is also valid
+    summary = Bot: My first test edit with pywikibot
+
+Every script has its own section with the script name as header.
+
+In addition the following generators and filters are supported but
+cannot be set by settings file:
+
+&params;
+"""
+#
+# (C) Pywikibot team, 2006-2021
+#
+# Distributed under the terms of the MIT license.
+#
+import pywikibot
+from pywikibot import pagegenerators
+from pywikibot.backports import Tuple
+from pywikibot.bot import (
+    AutomaticTWSummaryBot,
+    ConfigParserBot,
+    ExistingPageBot,
+    NoRedirectPageBot,
+    SingleSiteBot,
+)
+
+
+# This is required for the text that is shown when you run this script
+# with the parameter -help.
+docuReplacements = {'&params;': pagegenerators.parameterHelp}  # noqa: N816
+
+
+class BasicBot(
+    # Refer pywikobot.bot for generic bot classes
+    SingleSiteBot,  # A bot only working on one site
+    ConfigParserBot,  # A bot which reads options from scripts.ini setting file
+    # CurrentPageBot,  # Sets 'current_page'. Process it in treat_page method.
+    #                  # Not needed here because we have subclasses
+    ExistingPageBot,  # CurrentPageBot which only treats existing pages
+    NoRedirectPageBot,  # CurrentPageBot which only treats non-redirects
+    AutomaticTWSummaryBot,  # Automatically defines summary; needs summary_key
+):
+
+    """
+    An incomplete sample bot.
+
+    :ivar summary_key: Edit summary message key. The message that should be
+        used is placed on /i18n subdirectory. The file containing these
+        messages should have the same name as the caller script (i.e. basic.py
+        in this case). Use summary_key to set a default edit summary message.
+
+    :type summary_key: str
+    """
+
+    summary_key = 'basic-changing'
+
+    update_options = {
+        'replace': False,  # delete old text and write the new text
+        'summary': None,  # your own bot summary
+        'text': 'Test',  # add this text from option. 'Test' is default
+        'top': False,  # append text on top of the page
+        'outpage': u'User:mastiBot/test',  # default output page
+        'maxlines': 1000,  # default number of entries per page
+        'test': False,  # print testoutput
+        'negative': False,  # if True negate behavior i.e. mark pages that DO NOT contain search string
+    }
+
+    def run(self):
+        header = u"Ta strona jest okresowo uaktualniana przez [[Wikipedysta:MastiBot|MastiBota]]. Ostatnia aktualizacja ~~~~~. \n"
+        header += u"Wszelkie uwagi proszę zgłaszać w [[Dyskusja_Wikipedysty:Masti|dyskusji operatora]].\n\n"
+
+        reflinks = []  # initiate list
+        counter = 0
+        marked = 0
+        for tpage in self.generator:
+            counter += 1
+            if self.opt.test:
+                pywikibot.output(u'Treating #%i (%i marked): %s' % (counter, marked, tpage.title()))
+            refs = self.treat(tpage)  # get (name)
+            # if self.opt.test:
+            # pywikibot.output(u'%s' % refs)
+            if refs:
+                reflinks.append(tpage.title(as_link=True))
+                marked += 1
+
+        footer = u'\n\nPrzetworzono ' + str(counter) + u' stron'
+
+        outputpage = self.opt.outpage
+
+        result = self.generateresultspage(reflinks, outputpage, header, footer)
+
+    def treat(self, page):
+        # search for imagelinks in page
+        # quit after first one
+        found = False
+        for i in page.imagelinks():
+            if self.opt.test:
+                pywikibot.output(i.title())
+            if not self.excludedImage(i.title()):
+                found = True
+                break
+        if found:
+            return (None)
+        else:
+            return (page.title)
+
+    def excludedImage(self, title):
+        exclusions = ('flag', 'map', 'stamp', 'pictogram', 'ensign', 'medal', 'logo')
+        for e in exclusions:
+            if e in title.lower():
+                return (True)
+        return (False)
+
+    def generateresultspage(self, redirlist, pagename, header, footer):
+        """
+        Generates results page from redirlist
+        Starting with header, ending with footer
+        Output page is pagename
+        """
+        maxlines = int(self.opt.maxlines)
+        finalpage = header
+        if self.opt.test:
+            pywikibot.output(u'GENERATING RESULTS')
+        for p in redirlist:
+            if self.opt.test:
+                pywikibot.output(p)
+            finalpage += u'\n# ' + p
+
+        finalpage += footer
+        outpage = pywikibot.Page(pywikibot.Site(), pagename)
+        outpage.text = finalpage
+        if self.opt.test:
+            pywikibot.output(outpage.title())
+
+        outpage.save(summary=self.opt.summary)
+        return (True)
+
+
+def main(*args: Tuple[str, ...]) -> None:
+    """
+    Process command line arguments and invoke bot.
+
+    If args is an empty list, sys.argv is used.
+
+    :param args: command line arguments
+    """
+    options = {}
+    # Process global arguments to determine desired site
+    local_args = pywikibot.handle_args(args)
+
+    # This factory is responsible for processing command line arguments
+    # that are also used by other scripts and that determine on which pages
+    # to work on.
+    gen_factory = pagegenerators.GeneratorFactory()
+
+    # Process pagegenerators arguments
+    local_args = gen_factory.handle_args(local_args)
+
+    # Parse your own command line arguments
+    for arg in local_args:
+        arg, sep, value = arg.partition(':')
+        option = arg[1:]
+        if option in ('summary', 'text', 'outpage', 'maxlines'):
+            if not value:
+                pywikibot.input('Please enter a value for ' + arg)
+            options[option] = value
+        # take the remaining options as booleans.
+        # You will get a hint if they aren't pre-defined in your bot class
+        else:
+            options[option] = True
+
+    # The preloading option is responsible for downloading multiple
+    # pages from the wiki simultaneously.
+    gen = gen_factory.getCombinedGenerator(preload=True)
+    if gen:
+        # pass generator and private options to the bot
+        bot = BasicBot(generator=gen, **options)
+        bot.run()  # guess what it does
+    else:
+        pywikibot.bot.suggest_help(missing_generator=True)
+
+
+if __name__ == '__main__':
+    main()
