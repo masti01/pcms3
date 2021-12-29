840b12a27fdf5927a4dcebc3f087802f937f934d
masti01 <mastigm@gmail.com>
2021-12-29 23:46:15 +0100

    bulgaria.py: refactor

--- bulgaria.py
+++ bulgaria.py
@@ -30,7 +30,7 @@ add any option you want to change to that settings file below the [basic]
 section like:
 
     [basic] ; inline comments starts with colon
-    # This is a commend line. Assignments may be done with '=' or ':'
+    # This is a comment line. Assignments may be done with '=' or ':'
     text: A text with line break and
         continuing on next line to be put
     replace: yes ; yes/no, on/off, true/false and 1/0 is also valid
@@ -82,8 +82,8 @@ class Demography():
     def __str__(self):
         return '[[{self.name}]]: EKATTE:{self.ekatte_id} SID:{self.search_id} -> {self.demo_data}'.format(self=self)
 
-    def _search_page(self,ekatteID):
-        quote_page = 'https://nsi.bg/nrnm/index.php?ezik=en&f=9&search=%s' % ekatteID
+    def _search_page(self, ekatteid):
+        quote_page = 'https://nsi.bg/nrnm/index.php?ezik=en&f=9&search=%s' % ekatteid
         web = Request(quote_page, headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(web, timeout=10).read()
 
@@ -91,17 +91,14 @@ class Demography():
         sid = sidR.search(str(webpage)).group('sid')
         pywikibot.output('SID:{}'.format(sid))
 
-        return(sid)
+        return sid
 
-    def _demo_data(self,sid):
+    def _demo_data(self, sid):
         data = []
         quote_page = 'https://nsi.bg/nrnm/show9.php?sid=%s&ezik=en' % sid
         web = Request(quote_page, headers={'User-Agent': 'Mozilla/5.0'})
         webpage = urlopen(web, timeout=10).read()
 
-        # drowR = re.compile(r'cmid\"\>[\d\.]*?(?P<year>\d{4})\<\/td\>\n.*?ind\"\>(?P<population>\d*)<\/td\>\n.*?ind\"\>Census')
-        # drowR = re.compile(
-        #    r'<tr>\n\s*?<td class=\"cmid\">.*?(?P<year>\d{4})</td>\n\s*?<td class=\"ind\">.*?(?P<population>\d*?)</td>\n\s*?<td class=\"ind\">Census.*?</td>\n</tr>')
         drowR = re.compile(r'(?is)<tr>\\n(?P<table>.*?)\\n</tr>')
         datesR = re.compile(r'(?i).*?(?P<year>\d{4})</td>\\n.*?(?P<population>\d*)</td>\\n.*?Census')
         for dr in drowR.finditer(str(webpage)):
@@ -109,9 +106,9 @@ class Demography():
             dates = datesR.search(dr.group('table'))
             if dates:
                 # pywikibot.output('Dates::%s' % str(dates))
-                data.append((dates.group('year'),dates.group('population')))
+                data.append((dates.group('year'), dates.group('population')))
 
-        return(data)
+        return (data)
 
     @property
     def demo_template(self):
@@ -122,9 +119,12 @@ class Demography():
         count = 0
         for year, pop in self.demo_data:
             count += 1
-            text += f'\n | rok{count} = {year} || pop{count} = {pop}'
+            text += '\n | rok{} = {} || pop{} = {}'.format(count, year, count, pop)
 
-        text += '\n| źródło = Narodowy Instytut Statystyczny<ref>{{Cytuj stronę | url = https://www.nsi.bg/nrnm/show9.php?sid=%s&ezik=en | tytuł = NATIONAL REGISTER OF POPULATED PLACES | opublikowany = Narodowy Instytut Statystyczny Bułgarii | język = en | data dostępu = 2021-12-28}}</ref>' % self.search_id
+        text += '\n| źródło = Narodowy Instytut Statystyczny<ref>{{Cytuj stronę | url = ' \
+                'https://www.nsi.bg/nrnm/show9.php?sid=%s&ezik=en | tytuł = NATIONAL REGISTER OF POPULATED PLACES | ' \
+                'opublikowany = Narodowy Instytut Statystyczny Bułgarii | język = en | data dostępu = ' \
+                '2021-12-28}}</ref>' % self.search_id
         text += '\n}}'
 
         return text
@@ -140,9 +140,7 @@ class BasicBot(
     NoRedirectPageBot,  # CurrentPageBot which only treats non-redirects
     AutomaticTWSummaryBot,  # Automatically defines summary; needs summary_key
 ):
-
     """
-
     :ivar summary_key: Edit summary message key. The message that should be
         used is placed on /i18n subdirectory. The file containing these
         messages should have the same name as the caller script (i.e. basic.py
@@ -166,7 +164,8 @@ class BasicBot(
     def run(self):
         """TEST"""
         outputpage = self.opt.outpage
-        pywikibot.output(u'OUTPUTPAGE:%s' % outputpage)
+        if self.opt.test:
+            pywikibot.output(u'OUTPUTPAGE:%s' % outputpage)
 
         for p in self.generator:
             pywikibot.output(u'Treating: %s' % p.title())
@@ -177,46 +176,50 @@ class BasicBot(
         for p in ekatte_list:
             pywikibot.output(ekatte_list[p])
             page.text += ekatte_list[p].demo_template
-            page.text += f'\n* {ekatte_list[p]}'
+            page.text += '\n* {ekatte_list[p]}'
 
         page.save(summary=self.opt.summary)
         return
 
-    def treat(self, page) -> None:
-        """Load the given page, do some changes, and save it."""
+    def ekatte_id(self, page):  # get EKATTE ID from WikiData
         wd = page.data_item()
-        # print(type(wd))
 
-        # pywikibot.output('WDItem:{}'.format(wd))
-        # pywikibot.output('WDItem title:{}'.format(wd.title()))
         wd.get()
 
-        ekatteR = re.compile('\d+?')
+        ekatteR = re.compile('\d+?')  # new EKATTE is digits only
         try:
-            claims3990 = wd.claims['P3990'] # get EKATTE ID
+            claims3990 = wd.claims['P3990']  # get EKATTE ID
             for c in claims3990:
                 value = c.getTarget()
                 if ekatteR.match(value):
-                    pywikibot.output("Page {}, value:{}".format(page.title(as_link=True), value))
-                    ekatte[page.title()] = value
+                    if self.opt.test:
+                        pywikibot.output("Page {}, value:{}".format(page.title(as_link=True), value))
+                    return value
                 else:
                     if page.title() not in ekatte.keys():
-                        pywikibot.output("Page {}, WRONG value:{}".format(page.title(as_link=True), value))
-                        ekatte[page.title()] = None
+                        if self.opt.test:
+                            pywikibot.output("Page {}, WRONG value:{}".format(page.title(as_link=True), value))
+                        return None
         except:
-            pywikibot.output("Page {}, no EKATTE".format(page.title(as_link=True)))
-            ekatte[page.title()] = None
+            if self.opt.test:
+                pywikibot.output("Page {}, no EKATTE".format(page.title(as_link=True)))
+            return None
 
-        demo = Demography(page.title(),ekatte[page.title()],opt=True)
+    def treat(self, page) -> None:
+        """Load the given page, do some changes, and save it."""
 
-        pywikibot.output("Demo:{}".format(demo))
+        ekatte[page.title()] = self.ekatte_id(page)
+
+        demo = Demography(page.title(), ekatte[page.title()], opt=True)
+
+        if self.opt.test:
+            pywikibot.output("Demo:{}".format(demo))
         ekatte_list[page.title()] = demo
 
-        return #remove after preparing to update each page
+        return  # remove after preparing to update each page
         # if summary option is None, it takes the default i18n summary from
         # i18n subdirectory with summary_key as summary key.
         # self.put_current(text, summary=self.opt.summary)
-
 
 
 def main(*args: str) -> None:
