14fb6ced96287fc2e1cf3fedffe438093105e16d
masti <mastigm@gmail.com>
2023-10-25 18:21:41 +0200

    new script update

--- ms-mostwanted.py
+++ ms-mostwanted.py
@@ -200,7 +200,7 @@ class BasicBot(
                     finalpage += '{{Edytuj|%s|%s}}' % (nakedtitle, nakedtitle)
                 else:
                     finalpage += re.sub(r'\[\[', '[[:', title, count=1)
-                finalpage += f' || {res[i]}'
+                finalpage += f' || {redirlist[i]}'
             else:
                 if self.opt.edit:
                     nakedtitle = re.sub(r'\[\[|\]\]', '', title)
