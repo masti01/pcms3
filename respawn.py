5c7c8cfe3b0fbf7e7193827e695d2459d67ca4ee
masti <mastigm@gmail.com>
2024-04-02 22:31:30 +0200

    respawn: add -force

--- respawn.py
+++ respawn.py
@@ -13,37 +13,42 @@ proceses = {
     'ircartcounter-tr': 'masti/artnos tr &',
 }
 
-local_args = pywikibot.handle_args(args)
-for arg in local_args:
-    if arg ==  '-force':
-        force = True
-    else:
-        force = False
 
-# enable logging
-with open("masti/pid/respawn.log", "a") as logfile:
+def main(*args):
+    local_args = pywikibot.handle_args(args)
+    for arg in local_args:
+        if arg ==  '-force':
+            force = True
+        else:
+            force = False
 
-    # run for all processes
-    for pname in proceses.keys():
+    # enable logging
+    with open("masti/pid/respawn.log", "a") as logfile:
 
-        # get pidfile name
-        pidname = f'masti/pid/{pname}.pid'
+        # run for all processes
+        for pname in proceses.keys():
 
-        try:
-            file = open(pidname, "r")
-            pid = int(file.readline())
-            file.close()
-        except FileNotFoundError:
-            logfile.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (File:{pidname} NOT FOUND\n')
-            pid = None
+            # get pidfile name
+            pidname = f'masti/pid/{pname}.pid'
 
-        if force:
-            print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (PID:{pid}) killing...')
-            logfile.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (PID:{pid}) killing...\n')
-            os.system(f'kill -9 {pid}')
+            try:
+                file = open(pidname, "r")
+                pid = int(file.readline())
+                file.close()
+            except FileNotFoundError:
+                logfile.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (File:{pidname} NOT FOUND\n')
+                pid = None
 
-        # check if proper process still running
-        if pid not in psutil.pids():
-            print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (PID:{pid}) is dead... respawning...')
-            logfile.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (PID:{pid}) is dead... respawning...\n')
-            os.system(f'{proceses[pname]}')
+            if force:
+                print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (PID:{pid}) killing...')
+                logfile.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (PID:{pid}) killing...\n')
+                os.system(f'kill -9 {pid}')
+
+            # check if proper process still running
+            if pid not in psutil.pids():
+                print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (PID:{pid}) is dead... respawning...')
+                logfile.write(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")} {pname} (PID:{pid}) is dead... respawning...\n')
+                os.system(f'{proceses[pname]}')
+
+if __name__ == '__main__':
+    main()
