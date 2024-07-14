3d85e3124a28c66d229895f01a08c1cde1d763cf
masti <mastigm@gmail.com>
2024-07-14 20:56:34 +0200

    wlc: exception by Revsson

--- wlc3.py
+++ wlc3.py
@@ -3143,6 +3143,7 @@ ignorelist = [
     re.compile('.*[\./@]jeuxvideo\.com'),  # bot rejected on site (masti, Revsson)
     re.compile('.*[\./@]audio\.art\.pl/2013/index.php'),  # bot rejected on site (masti, Revsson)
     re.compile('.*[\./@]bfm\.ru'),  # bot rejected on site (masti, NiktWażny)
+    re.compile('.*[\./@]lccn\.loc\.gov'),  # bot rejected on site (masti, Revsson)
 ]
 
 
