8b55549fb83bbeb14560fb0cce44cfd2595a4947
masti <mastigm@gmail.com>
2024-10-02 21:35:36 +0200

    test script

--- section-test.py
+++ section-test.py
@@ -57,7 +57,7 @@ from pywikibot.bot import (
     SingleSiteBot,
 )
 
-from pywikibot.backports import Tuple
+# from pywikibot.backports import Tuple
 from pywikibot import pagegenerators
 from pywikibot import textlib
 
