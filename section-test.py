e5057ead2b7e2de85b3ad69db61fdd6767838af0
masti <mastigm@gmail.com>
2024-10-02 21:36:14 +0200

    test script

--- section-test.py
+++ section-test.py
@@ -74,7 +74,7 @@ class BasicBot(
     # CurrentPageBot,  # Sets 'current_page'. Process it in treat_page method.
     #                  # Not needed here because we have subclasses
     ExistingPageBot,  # CurrentPageBot which only treats existing pages
-    NoRedirectPageBot,  # CurrentPageBot which only treats non-redirects
+    # NoRedirectPageBot,  # CurrentPageBot which only treats non-redirects
     AutomaticTWSummaryBot,  # Automatically defines summary; needs summary_key
 ):
 
