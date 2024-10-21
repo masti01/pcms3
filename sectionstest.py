8b617683a25427a46f0d5552b163d96384f05de2
masti <mastigm@gmail.com>
2024-10-21 21:16:28 +0200

    test script

--- sectionstest.py
+++ sectionstest.py
@@ -124,7 +124,7 @@ class BasicBot(
             for s in pagesections.sections:
                 pywikibot.output(f'Section title:{s.title}')
                 pywikibot.output(f'Section content:{s.content}')
-                if 'linki zewnetrzne' in s.title.lower():
+                if 'Linki zewnetrzne' in s.title:
                     pywikibot.output(f'Found Section content:{s.content}')
 
         return
