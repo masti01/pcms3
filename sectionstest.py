dd4c2b33df5a1733bbffbffc5cda40542762d425
masti <mastigm@gmail.com>
2025-04-22 14:39:18 +0200

    test

--- sectionstest.py
+++ sectionstest.py
@@ -190,7 +190,7 @@ class BasicBot(
         for t in parsed.filter_templates():
             pywikibot.output(str(t))
             link = str(t.get('link').value).rstrip() + '\n'
-            linkR = re.compile(r'(?si)\*?\s*?(?P<link>h[^\s]*?)\s*(?P<archiwum>\(\[.*?\]\))?\n(?P<history>\s*\*\*.*?\n)*')
+            linkR = re.compile(r'(?si)\*?\s*?(?P<link>h[^\s]*?)\s*(?P<archiwum>\(\[.*?\]\))?\n(?P<history>.*)')
             pywikibot.output(f'LINK:{link}')
             m = re.search(linkR, link)
             newlink = m.group('link')
