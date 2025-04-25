ec0bdd542486d1709969025d48bb28e8f70452e2
masti <mastigm@gmail.com>
2025-04-25 12:10:20 +0200

    messaging

--- cleanmld.py
+++ cleanmld.py
@@ -138,14 +138,14 @@ class BasicBot(
                 link = str(t.get('link').value).rstrip() + '\n'
                 linkR = re.compile(r'(?si)\*?\s*?(?P<link>http[^\s]*?)( [^\n]*)?\n(?P<history>.*)')
                 if self.opt.test:
-                    pywikibot.debug(f'LINK:{link}')
+                    pywikibot.output(f'LINK:{link}')
                 m = re.search(linkR, link)
                 try:
                     newlink = m.group('link')
                 except AttributeError:
                     newlink = ''
                     self.opt.errorcounter += 1
-                    pywikibot.debug(f'ERROR (MLD) #{self.opt.errorcounter}: no link in template on page:{self.current_page.title(as_link=True)}')
+                    pywikibot.output(f'ERROR (MLD) #{self.opt.errorcounter}: no link in template on page:{self.current_page.title(as_link=True)}')
 
                 if t.has('historia'):
                     pywikibot.output(f'historia= param found')
@@ -165,14 +165,14 @@ class BasicBot(
                     except AttributeError:
                         newhistory = ''
                         self.opt.errorcounter += 1
-                        pywikibot.debug(f'ERROR (MLD) #{self.opt.errorcounter}: no history in template on page:{self.current_page.title(as_link=True)}')
+                        pywikibot.output(f'ERROR (MLD) #{self.opt.errorcounter}: no history in template on page:{self.current_page.title(as_link=True)}')
 
                 try:
                     IA = str(t.get('IA').value).rstrip()
                 except ValueError:
                     IA = ''
                     self.opt.errorcounter += 1
-                    pywikibot.debug(f'ERROR (MLD) #{self.opt.errorcounter}: no IA param in template on page:{self.current_page.title(as_link=True)}')
+                    pywikibot.output(f'ERROR (MLD) #{self.opt.errorcounter}: no IA param in template on page:{self.current_page.title(as_link=True)}')
                 # generate new template version
                 # if self.opt.test:
                 #     t2 = f'{{{{Wikipedysta:Masti/mld\n| link = {newlink}\n| IA = {IA}\n| historia ={newhistory}}}}}'
