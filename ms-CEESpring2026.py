83d4b3205f832836ccf2e82c41942ee9e259d74c
masti <mastigm@gmail.com>
2026-05-17 21:29:59 +0200

    countries

--- ms-CEESpring2026.py
+++ ms-CEESpring2026.py
@@ -584,7 +584,7 @@ countryNames = {
            'Саха (Якутия)': 'Sakha', 'Саха': 'Sakha', 'Татарстан Республикасы': 'Tatars', 'эрзя': 'Erzia',
            'Югары Лужица': 'Sorbia', 'Үзбәкстан': 'Uzbekistan', 'Таҗикстан': 'Tajikistan', 'Иран': 'Iran',
            'Чуашлар': 'Chuvasz', 'кырым татарлары': 'Crimean Tatars', 'Чуашстан': 'Chuvasz',
-           'Кыргызстан': 'Kyrgyzstan',
+           'Кыргызстан': 'Kyrgyzstan', 'Рәсәй Федерациясе': 'Russia',
            },
     # tr countries
     'tr': {'Arnavutluk': 'Albania', 'Avusturya': 'Austria', 'Azerbaycan': 'Azerbaijan', 'Başkurdistan': 'Bashkortostan',
