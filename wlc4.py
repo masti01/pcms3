f49ca9bf2c33bf2e3f025db8d6a611e0061e8c00
masti <mastigm@gmail.com>
2026-08-23 11:48:04 +0200

    exceptions

--- wlc4.py
+++ wlc4.py
@@ -3072,6 +3072,8 @@ ignorelist = [
     re.compile(r'.*[\./@]mark-weeks\.com/chess'),  # bot rejected with illegal code on site (NiktWażny, masti)
     re.compile(r'.*[\./@]dataverse\.harvard\.edu/dataset\.xhtml'),  # bot rejected on site (NiktWażny, masti)
     re.compile(r'.*[\./@]ew\.com/article/1991/03/29/bob-dylans-discography/'),  # bot rejected on site (NiktWażny, masti)
+    re.compile(r'.*[\./@]dib\.ie/biography/'),  # unsopported redirect on site (Elfhelm, masti)
+    re.compile(r'.*[\./svobodnaevropa\.bg/a/31194607\.html'),  # (Elfhelm, masti)
 ]
 
 
